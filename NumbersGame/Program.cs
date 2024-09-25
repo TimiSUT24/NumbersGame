5f17c877302411353714906dcf15692b1839101b
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2024-09-25 21:00:16 +0200

    skapar ny metod Checkguess

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -4,7 +4,11 @@ namespace NumbersGame
 {
 
     public class Program
-    {
+    {   
+        static bool Checkguess(int user, int number, int guessLeft)
+        {
+
+        }
 
         static void Main(string[] args)
         {
